666cc9bc5980a7d79f96b86e539a8b31ffb070e3
anibalfn <anibalneto29@gmail.com>
2024-02-08 20:39:10 -0300

    Add logic properties and types

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
   styleUrl: './quiz.component.css'
 })
-export class QuizComponent {
+export class QuizComponent implements OnInit {
+  title: string = '';
+  questions: any;
+  selectedQuestion: any;
+  answers: string[] = [];
+  selectedAnswer: string = '';
+
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
+
+  ifFinished: boolean = false;
+
+  ngOnInit(): void {
+
+  }
+
 
 }
