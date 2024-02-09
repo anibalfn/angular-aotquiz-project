576d7fdfa2ea37831fef94a862136aac8a56dec4
anibalfn <anibalneto29@gmail.com>
2024-02-08 21:12:40 -0300

    Add game rules

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import quiz_questions from "../../../assets/data/quiz_questions.json";
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
@@ -18,7 +18,31 @@ export class QuizComponent implements OnInit {
   ifFinished: boolean = false;
 
   ngOnInit(): void {
+    if (quiz_questions) {
+      this.ifFinished = false;
+      this.title = quiz_questions.title;
 
+      this.questions = quiz_questions.questions;
+      this.selectedQuestion = this.questions[this.questionIndex];
+
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
+
+    }
+  }
+
+  pressedButton(value: string) {
+    this.answers.push(value)
+  }
+
+  async nextQuestion() {
+    this.questionIndex += 1;
+
+    if (this.questionMaxIndex > this.questionIndex) {
+      this.selectedQuestion = this.questions[this.questionIndex]
+    } else {
+      this.ifFinished = true;
+    }
   }
 
 
